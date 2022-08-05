3e1e33330126f7ae4a98921d0551995ad09b291e
hdi0b <977868425@qq.com>
2022-08-05 15:54:14 +0800

    add: 62.不同路径.go

---
+++ 62.不同路径.go
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=golang
+ *
+ * [62] 不同路径
+ */
+package leetcode
+
+// @lc code=start
+func uniquePaths(m int, n int) int {
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i == 0 && j == 0 {
+				dp[i][j] = 1
+			} else if i == 0 {
+				dp[i][j] = dp[i][j-1]
+			} else if j == 0 {
+				dp[i][j] = dp[i-1][j]
+			} else {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
+			}
+
+		}
+	}
+
+	return dp[m-1][n-1]
+}
+
+// @lc code=end
