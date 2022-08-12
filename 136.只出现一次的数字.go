c3dd175ac2c5603fedb8192e9e5b67645cf78caf
hdi0b <977868425@qq.com>
2022-08-12 20:45:12 +0800

    add: 136.只出现一次的数字.go

---
+++ 136.只出现一次的数字.go
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=golang
+ *
+ * [136] 只出现一次的数字
+ */
+package leetcode
+
+// @lc code=start
+
+// 位运算-异或
+func singleNumber(nums []int) int {
+	var ans int
+
+	for _, v := range nums {
+		ans ^= v
+	}
+
+	return ans
+}
+
+// @lc code=end
