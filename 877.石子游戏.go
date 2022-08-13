947a8f75cbf1eaea4814cb99b3e236b7983f767f
hdi0b <977868425@qq.com>
2022-08-13 10:46:24 +0800

    add: 877.石子游戏.g

---
+++ 877.石子游戏.go
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=877 lang=golang
+ *
+ * [877] 石子游戏
+ */
+package leetcode
+
+// @lc code=start
+func stoneGame(piles []int) bool {
+	// 偶数堆, 两玩家要么所有偶数堆要么所有奇数堆
+	// 先手一定赢
+	return true
+}
+
+// @lc code=end
