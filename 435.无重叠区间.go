e1429624a6f5d934152eca06730f6ea6ce3157eb
hdi0b <977868425@qq.com>
2022-08-14 10:30:07 +0800

    add: 435.无重叠区间.go

---
+++ 435.无重叠区间.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=435 lang=golang
+ *
+ * [435] 无重叠区间
+ */
+package leetcode
+
+import (
+	"sort"
+)
+
+// @lc code=start
+func eraseOverlapIntervals(intervals [][]int) int {
+	// 按每个区间的结束排序
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][1] < intervals[j][1]
+	})
+	ans := 0
+	l, r := intervals[0][0], intervals[0][1]
+	for _, v := range intervals[1:] {
+		if v[0] < r && v[1] > l {
+			ans++
+
+		} else {
+			r = v[1]
+		}
+	}
+
+	return ans
+}
+
+// @lc code=end
