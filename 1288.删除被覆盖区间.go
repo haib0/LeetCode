b6bf2fca3d0c1082d941f2879c536171738940e2
hdi0b <977868425@qq.com>
2022-08-14 10:55:23 +0800

    fix: 1288-go

--- 1288.删除被覆盖区间.go
+++ 1288.删除被覆盖区间.go
@@ -10,6 +10,9 @@ import "sort"
 // @lc code=start
 func removeCoveredIntervals(intervals [][]int) int {
 	sort.Slice(intervals, func(i, j int) bool {
+		if intervals[i][0] == intervals[j][0] {
+			return intervals[i][1] > intervals[j][1]
+		}
 		return intervals[i][0] < intervals[j][0]
 	})
 	nDel := 0
