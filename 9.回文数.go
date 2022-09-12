a7b94a13f23746b37d01d46ec63d72e0cdf571b1
hdi0b <977868425@qq.com>
2022-09-12 19:25:17 +0800

    add: 9.回文数.go

---
+++ 9.回文数.go
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=golang
+ *
+ * [9] 回文数
+ */
+package leetcode
+
+// @lc code=start
+func isPalindrome(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+	var rN int
+	for x > rN {
+		rN = rN*10 + x%10
+		x /= 10
+	}
+	return x == rN || x == rN/10
+}
+
+// @lc code=end
