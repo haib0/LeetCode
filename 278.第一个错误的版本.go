8fa40cfbb7986a33af37aef67e6dce869cd075a0
hdi0b <977868425@qq.com>
2022-10-04 19:46:11 +0800

    add: 278.第一个错误的版本.go

---
+++ 278.第一个错误的版本.go
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=278 lang=golang
+ *
+ * [278] 第一个错误的版本
+ */
+package leetcode
+
+// @lc code=start
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+	lo, hi := 1, n
+	for lo < hi {
+		mid := (lo + hi) / 2
+		if isBadVersion(mid) {
+			hi = mid
+		} else {
+			lo = mid + 1
+		}
+	}
+	return lo
+}
+
+// @lc code=end
