6a96958224ebd78af5fb498ee1324d1cea4544d0
hdi0b <977868425@qq.com>
2022-10-12 15:30:27 +0800

    fmt 79.单词搜索.go

--- 79.单词搜索.go
+++ 79.单词搜索.go
@@ -7,33 +7,35 @@
 package leetcode
 
 // @lc code=start
+
 func exist(board [][]byte, word string) bool {
+	var dfs func(board [][]byte, word string, i, j int) bool
+	dfs = func(board [][]byte, word string, i, j int) bool {
+		if i < 0 || i >= len(board) || j < 0 || j >= len(board[0]) {
+			return false
+		}
+		if board[i][j] == byte('0') {
+			return false
+		}
+		if board[i][j] == word[0] {
+			cache := board[i][j]
+			board[i][j] = byte('0')
+			if len(word) == 1 {
+				return true
+			} else {
+				r := dfs(board, word[1:], i+1, j) || dfs(board, word[1:], i, j+1) || dfs(board, word[1:], i-1, j) || dfs(board, word[1:], i, j-1)
+				board[i][j] = cache
+				return r
+			}
+		}
+		return false
+	}
+
 	for i := 0; i < len(board); i++ {
 		for j := 0; j < len(board[0]); j++ {
 			if dfs(board, word, i, j) {
 				return true
 			}
-		}
-	}
-	return false
-}
-
-func dfs(board [][]byte, word string, i, j int) bool {
-	if i < 0 || i >= len(board) || j < 0 || j >= len(board[0]) {
-		return false
-	}
-	if board[i][j] == byte('0') {
-		return false
-	}
-	if board[i][j] == word[0] {
-		cache := board[i][j]
-		board[i][j] = byte('0')
-		if len(word) == 1 {
-			return true
-		} else {
-			r := dfs(board, word[1:], i+1, j) || dfs(board, word[1:], i, j+1) || dfs(board, word[1:], i-1, j) || dfs(board, word[1:], i, j-1)
-			board[i][j] = cache
-			return r
 		}
 	}
 	return false
